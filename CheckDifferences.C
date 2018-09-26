e4bfe7e0ec2da42e421db0a91e25a0c3b94526db
esallenback <33500930+esallenback@users.noreply.github.com>
2018-09-26 16:52:44 -0500

    Update CheckDifferences.C

--- CheckDifferences.C
+++ CheckDifferences.C
@@ -14,8 +14,7 @@
 #include "TH1.h"
 #include "TRandom3.h"
 #include <getopt.h>
-
-int main(int argc, char* argv[])
+ int main(int argc, char* argv[])
 {               
     if (argc-1!= 2)
     {
@@ -26,8 +25,7 @@ int main(int argc, char* argv[])
     }
   const char *inputFileList1 = argv[1];
   const char *inputFileList2 = argv[2];
-
-  if (strstr(inputFileList1, "LPC") != NULL)
+   if (strstr(inputFileList1, "LPC") != NULL)
     {
       std::cout<<"Please put UCSB as the first file and LPC as the second"<<std::endl;
       return -1;
@@ -45,14 +43,12 @@ int main(int argc, char* argv[])
       std::cerr << "** ERROR: Can't open UCSB Dataset for input" << std::endl;
       return false;
     }
-
-  if(!inputLPC.is_open())
+   if(!inputLPC.is_open())
     {
       std::cerr << "** ERROR: Can't open LPC Dataset for input" << std::endl;
       return false;
     }
-
-  for(std::string line; getline(inputUCSB, line);)
+   for(std::string line; getline(inputUCSB, line);)
     {
       if (line[0] == '#') continue;
       std::cout << "Add File: " << line << std::endl;
@@ -64,8 +60,7 @@ int main(int argc, char* argv[])
       std::cout << "Add File: " << line << std::endl;
       LPCChain->Add(line.c_str());
     }
-
-  //std::cout << "No. of Entries in this tree : " << fChain->GetEntries() << std::endl;
+   //std::cout << "No. of Entries in this tree : " << fChain->GetEntries() << std::endl;
                                                        
   NTupleReader trUCSB(UCSBChain);
   NTupleReader trLPC(LPCChain);
@@ -81,100 +76,106 @@ int main(int argc, char* argv[])
       std::cout << "These two Ntuples do not have the same number of entries" << std::endl;
       return -1;
     }
-
-  using namespace std;
-
-  //create files
+   using namespace std;
+   //create files
   ofstream outputFileLPCPt;
-  outputFileLPCPt.open("LPCvetoElectronIDPt.csv");
+  outputFileLPCPt.open("LPCDifflooseElectronIDPt.csv");
   ofstream outputFileLPCEta;
-  outputFileLPCEta.open("LPCvetoElectronIDEta.csv");
+  outputFileLPCEta.open("LPCDifflooseElectronIDEta.csv");
   ofstream outputFileLPCPhi;
-  outputFileLPCPhi.open("LPCvetoElectronIDPhi.csv");
+  outputFileLPCPhi.open("LPCDifflooseElectronIDPhi.csv");
   ofstream outputFileUCSBPt;
-  outputFileUCSBPt.open("UCSBvetoElectronIDPt.csv");
+  outputFileUCSBPt.open("UCSBDifflooseElectronIDPt.csv");
   ofstream  outputFileUCSBEta;
-  outputFileUCSBEta.open("UCSBvetoElectronIDEta.csv");
+  outputFileUCSBEta.open("UCSBDifflooseElectronIDEta.csv");
   ofstream outputFileUCSBPhi;
-  outputFileUCSBPhi.open("UCSBvetoElectronIDPhi.csv");
+  outputFileUCSBPhi.open("UCSBDifflooseElectronIDPhi.csv");
   int count = 0;
-  outputFileUCSBPt << "Event,Column 1,Column 2,Column 3,Column 4,Column 5"<< endl;
-  outputFileUCSBEta << "Event,Column 1,Column 2,Column 3,Column 4,Column 5"<< endl;
-  outputFileUCSBPhi << "Event,Column 1,Column 2,Column 3,Column 4,Column 5"<< endl;
-  outputFileUCSBPt << "Event,Column 1,Column 2,Column 3,Column 4,Column 5"<< endl;
-  outputFileUCSBEta << "Event,Column 1,Column 2,Column 3,Column 4,Column 5"<<endl;
-  outputFileUCSBPhi << "Event,Column 1,Column 2,Column 3,Column 4,Column 5"<<endl;
-
-  //loop through all events
+   //loop through all events
   while(trLPC.getNextEvent())
     {
       //get LPC event number to find the same UCSB event number
       int eventNumber = trLPC.getVar<int>("event");
       bool sameEvent = false;
-
-      //Find the UCSB event that corresponds to the LPC event
+      bool different = true;
+       //Find the UCSB event that corresponds to the LPC event
       //Print UCSB event information to respective files
-      for(int i = 0; sameEvent!=true;i++){
+      for(int i = 0; sameEvent!=true and different==true;i++){
 	trUCSB.goToEvent(i);
 	if(trLPC.getVar<int>("event")==trUCSB.getVar<int>("event"))
-	  {
-	    int commaCountUCSB=0;
-	    const std::vector<int>& vetoElectronIDVecUCSB = trUCSB.getVec<int>("vetoElectronID");
-	    const std::vector<TLorentzVector>& elesLVecUCSB  = trUCSB.getVec<TLorentzVector>("elesLVec");
-	    outputFileUCSBPt << "Event "<< eventNumber;
-	    outputFileUCSBEta << "Event "<< eventNumber;
-	    outputFileUCSBPhi << "Event "<< eventNumber;
-	    //loops over to find vetoElectronID Pt, Phi, and Eta
-	    for(int i = 0; i < vetoElectronIDVecUCSB.size(); i++){
-	      if(vetoElectronIDVecUCSB[i] == 1){
-		outputFileUCSBPt << "," << elesLVecUCSB[i].Pt();
-		outputFileUCSBEta << "," << elesLVecUCSB[i].Eta();
-		outputFileUCSBPhi << "," << elesLVecUCSB[i].Phi();
-		commaCountUCSB++;
-	      }
-	    }
-	    while(commaCountUCSB!=5){
-	      outputFileUCSBPt << " ,";
-	      outputFileUCSBEta << " ,";
-	      outputFileUCSBPhi << " ,";
-	      commaCountUCSB++;
-	    }
-	    outputFileUCSBPt << endl;
-	    outputFileUCSBEta << endl;
-	    outputFileUCSBPhi << endl;
-	    sameEvent = true;
+	  {	
+		const std::vector<int>& looseElectronIDVecUCSB = trUCSB.getVec<int>("looseElectronID");
+	    	const std::vector<TLorentzVector>& elesLVecUCSB  = trUCSB.getVec<TLorentzVector>("elesLVec");
+	    	const std::vector<int>& looseElectronIDVecLPC = trLPC.getVec<int>("looseElectronID");      
+      	    	const std::vector<TLorentzVector>& elesLVecLPC  = trLPC.getVec<TLorentzVector>("elesLVec");
+      	    	for(int y = 0; y < looseElectronIDVecUCSB.size(); y++){
+	     		if(looseElectronIDVecUCSB[y] == 1){
+				bool sameExistsPt = false;
+				bool sameExistsEta = false;
+				bool sameExistsPhi = false;
+				for(int z = 0; z < looseElectronIDVecLPC.size(); z++){
+					if(looseElectronIDVecLPC[z]==1){
+					  if(elesLVecLPC[z].Pt()==elesLVecUCSB[y].Pt()){
+					    sameExistsPt = true;
+					  }
+					  if(elesLVecLPC[z].Eta()==elesLVecUCSB[y].Eta()){
+					    sameExistsEta = true;
+					  }
+					  if(elesLVecLPC[z].Phi()==elesLVecUCSB[y].Phi()){
+					    sameExistsPhi = true;
+					  }
+					}
+				}
+				if(sameExistsPt==false){
+					outputFileUCSBPt << "Event "<< eventNumber<<","<< elesLVecUCSB[y].Pt()<<endl;
+				}
+				if(sameExistsEta==false){
+					outputFileUCSBEta << "Event "<< eventNumber<<","<< elesLVecUCSB[y].Eta()<<endl;
+				}
+				if(sameExistsPhi==false){
+					outputFileUCSBPhi << "Event "<< eventNumber<<","<< elesLVecUCSB[y].Phi()<<endl;
+				}
+			}
+		}
+		for(int y = 0; y < looseElectronIDVecLPC.size(); y++){
+	     		if(looseElectronIDVecLPC[y] == 1){
+				bool sameExistsPt = false;
+				bool sameExistsEta = false;
+				bool sameExistsPhi = false;
+				for(int z = 0; z <looseElectronIDVecUCSB.size(); z++){
+				  if(looseElectronIDVecUCSB[z]==1){
+					  if(elesLVecUCSB[z].Pt()==elesLVecLPC[y].Pt()){
+					    sameExistsPt = true;
+					  }
+					  if(elesLVecUCSB[z].Eta()==elesLVecLPC[y].Eta()){
+					    sameExistsEta = true;
+					  }
+					  if(elesLVecUCSB[z].Phi()==elesLVecLPC[y].Phi()){
+					    sameExistsPhi = true;
+					  }
+				  }
+				}
+				if(sameExistsPt==false){
+				  outputFileLPCPt << "Event "<< eventNumber<<","<< elesLVecLPC[y].Pt()<<endl;
+				}
+				if(sameExistsEta==false){
+				  outputFileLPCEta << "Event "<< eventNumber<<","<< elesLVecLPC[y].Eta()<<endl;
+				}
+				if(sameExistsPhi==false){
+				  outputFileLPCPhi << "Event "<< eventNumber<<","<< elesLVecLPC[y].Phi()<<endl;
+				}
+			}
+		}
+		sameEvent = true;
 	  }
       }
-      const std::vector<int>& vetoElectronIDVecLPC = trLPC.getVec<int>("vetoElectronID");      
-      const std::vector<TLorentzVector>& elesLVecLPC  = trLPC.getVec<TLorentzVector>("elesLVec");
-      int commaCountLPC=0;
-      outputFileLPCPt << "Event "<< eventNumber;
-      outputFileLPCEta << "Event "<< eventNumber;
-      outputFileLPCPhi << "Event "<< eventNumber;
-      //loops over to find vetoElectronID Pt, Phi, and Eta                                                                               
-      for(int i = 0; i < vetoElectronIDVecLPC.size(); i++){
-	if(vetoElectronIDVecLPC[i] == 1){
-	  outputFileLPCPt << "," << elesLVecLPC[i].Pt();
-	  outputFileLPCEta << "," << elesLVecLPC[i].Eta();
-	  outputFileLPCPhi << "," << elesLVecLPC[i].Phi();
-	  commaCountLPC++;
-	}
-      }
-      while(commaCountLPC!=5){
-	outputFileLPCPt << ",";
-	outputFileLPCEta << ",";
-	outputFileLPCPhi << ",";
-	commaCountLPC++;
-      }
-      outputFileLPCPt << endl;
-      outputFileLPCEta << endl;
-      outputFileLPCPhi << endl;
-
       //Keeps track of count and prints onto screen for every 100 events processed
       count++;
-      if(count%100==0)
+      if(count%100==0){
 	std::cout<<count<<" done"<<std::endl;
+      }
     }
+	
   //Closes all output files
   outputFileLPCPt.close();
   outputFileLPCEta.close();
